cc55131f85c6e01e18753aa64793fa838d1fbdb9
KazuhikoTsuji <tsuji.kazuhiko.0222@gmail.com>
2020-02-16 14:56:15 +0900

    2020/02/16-01

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w. "Hello from the webserver")	
+  fmt.Fprintf(w, "Hello from the webserver")	
 }
 
 func main() {
